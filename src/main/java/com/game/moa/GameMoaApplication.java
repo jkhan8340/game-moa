dd552b6ef9eaf7c394c5e60d25ad52948be0e200
INCRUIT\hanjk <gkswjdrl123@naver.com>
2023-10-13 14:40:06 +0900

    initialize commit

---
+++ src/main/java/com/game/moa/GameMoaApplication.java
@@ -0,0 +1,13 @@
+package com.game.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/moa/GameMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

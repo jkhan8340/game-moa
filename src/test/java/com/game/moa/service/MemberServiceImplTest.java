04389da8e2efbe5789a26e8052594e04110bdbb6
INCRUIT\hanjk <gkswjdrl123@naver.com>
2023-10-16 17:21:53 +0900

    test commit

--- src/test/java/com/game/moa/service/MemberServiceImplTest.java
+++ src/test/java/com/game/moa/service/MemberServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.parameters.P;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
